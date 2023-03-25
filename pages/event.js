c099f369f8d32fc83424f62885232cfb633fac28
hk2257853 <hk2257853@gmail.com>
2023-03-25 19:57:26 +0530

    event page with luma registeration, sponsership section

---
+++ pages/event.js
@@ -0,0 +1,15 @@
+export default function Event() {
+    return (
+        <>
+        <a
+        href="https://lu.ma/event/evt-4LEqHzIVQizhJso"
+        class="luma-checkout--button"
+        data-luma-action="checkout"
+        data-luma-event-id="evt-4LEqHzIVQizhJso"
+        >
+        test button
+        </a>
+        <script id="luma-checkout" src="https://embed.lu.ma/checkout-button.js"></script>
+        </>
+    );
+}
