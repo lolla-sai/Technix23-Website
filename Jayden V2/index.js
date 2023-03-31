568bbc24991e240262586b26f41c92637803d5c1
Jayden Viegas <jaydenviegas57@gmail.com>
2023-03-31 11:21:29 +0530

    Responsive.css added

--- Jayden V2/index.js
+++ Jayden V2/index.js
@@ -1,49 +1,15 @@
-let eventSlider = document.getElementById('event_slider');
-eventSlider.addEventListener("scroll", onScrollEvent);
+if(window.innerWidth > 450) {
+    let eventSlider = document.getElementById('event_slider');
+    eventSlider.addEventListener("scroll", onScrollEvent);
 
-// Moves the skull when the event slider is scrolled
-function onScrollEvent(event) {
-    event.preventDefault()
-    let eventSliderMax = eventSlider.scrollWidth - eventSlider.clientWidth;
-    let rightEdge = slider.offsetWidth - skull.offsetWidth;
-    let newLeft = eventSlider.scrollLeft * (rightEdge / eventSliderMax);
-    skull.style.left = newLeft + 'px';
-    
-    if(newLeft == rightEdge) {
-        console.log(document.getElementById('slider').offsetWidth)
-        inslider.style.width = document.getElementById('slider').offsetWidth + 'px';
-    }
-    else {
-        inslider.style.width = newLeft + 24 + 'px';
-    }
-}
-
-// Portfolio Silder
-let skull = document.querySelector('#skull');
-let inslider = document.querySelector('#inslider');
-
-skull.onmousedown = function(event) {
-    event.preventDefault();
-
-    let shiftX = event.clientX - skull.getBoundingClientRect().left;
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-    
-    function onMouseMove(event) {
-        eventSlider.removeEventListener("scroll", onScrollEvent);
-        let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
-
-        if (newLeft < 0) {
-            newLeft = 0;
-        }
+    // Moves the skull when the event slider is scrolled
+    function onScrollEvent(event) {
+        event.preventDefault()
+        let eventSliderMax = eventSlider.scrollWidth - eventSlider.clientWidth;
         let rightEdge = slider.offsetWidth - skull.offsetWidth;
-        if (newLeft > rightEdge) {
-            newLeft = rightEdge;
-        }
-
+        let newLeft = eventSlider.scrollLeft * (rightEdge / eventSliderMax);
         skull.style.left = newLeft + 'px';
-
+        
         if(newLeft == rightEdge) {
             console.log(document.getElementById('slider').offsetWidth)
             inslider.style.width = document.getElementById('slider').offsetWidth + 'px';
@@ -51,22 +17,58 @@ skull.onmousedown = function(event) {
         else {
             inslider.style.width = newLeft + 24 + 'px';
         }
-
-        let eventSliderMax = eventSlider.scrollWidth - eventSlider.clientWidth;
-        eventSlider.scrollLeft = newLeft * (eventSliderMax / rightEdge);
     }
 
-    function addListener() {
-        eventSlider.addEventListener("scroll", onScrollEvent);
-    }
+    // Bottom Silder
+    let skull = document.querySelector('#skull');
+    let inslider = document.querySelector('#inslider');
 
-    function onMouseUp() {
-        setTimeout(addListener, 2000)
-        document.removeEventListener('mouseup', onMouseUp);
-        document.removeEventListener('mousemove', onMouseMove);
-    }
-};
+    skull.onmousedown = function(event) {
+        event.preventDefault();
 
-skull.ondragstart = function() {
-    return false;
-};
+        let shiftX = event.clientX - skull.getBoundingClientRect().left;
+
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+        
+        function onMouseMove(event) {
+            eventSlider.removeEventListener("scroll", onScrollEvent);
+            let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
+
+            if (newLeft < 0) {
+                newLeft = 0;
+            }
+            let rightEdge = slider.offsetWidth - skull.offsetWidth;
+            if (newLeft > rightEdge) {
+                newLeft = rightEdge;
+            }
+
+            skull.style.left = newLeft + 'px';
+
+            if(newLeft == rightEdge) {
+                console.log(document.getElementById('slider').offsetWidth)
+                inslider.style.width = document.getElementById('slider').offsetWidth + 'px';
+            }
+            else {
+                inslider.style.width = newLeft + 24 + 'px';
+            }
+
+            let eventSliderMax = eventSlider.scrollWidth - eventSlider.clientWidth;
+            eventSlider.scrollLeft = newLeft * (eventSliderMax / rightEdge);
+        }
+
+        function addListener() {
+            eventSlider.addEventListener("scroll", onScrollEvent);
+        }
+
+        function onMouseUp() {
+            setTimeout(addListener, 2000)
+            document.removeEventListener('mouseup', onMouseUp);
+            document.removeEventListener('mousemove', onMouseMove);
+        }
+    };
+
+    skull.ondragstart = function() {
+        return false;
+    };
+}
