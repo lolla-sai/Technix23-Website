9f99a18df5a693dfbf6ccc1385272d6b9a826e43
puneetmpatil <patilpuneet01@gmail.com>
2023-04-14 22:29:05 +0530

    Talk Images Added

---
+++ data/talks.js
@@ -0,0 +1,34 @@
+import { Serverless, Networking, Career, DecodingSecret, NewComputing } from "@/public/assets/images/Talks";
+const seminarCardData = [
+    {
+        "title": "Careers in IT",
+        "image": Career,
+        "linkedin": "https://www.linkedin.com/in/aniket-khorjuvekar-a31b1a30",
+        "registrationLink":"https://lu.ma/CareerInIt"
+    },
+    {
+        "title": "New Computing Abstractions",
+        "image": NewComputing,
+        "linkedin": "https://www.linkedin.com/in/kunal-korgaonkar-70171418",
+        "registrationLink":"https://lu.ma/ComputerAbstraction"
+    },
+    {
+        "title": "Networking 101",
+        "image": Networking,
+        "linkedin": "https://www.linkedin.com/in/maheshwari-terse",
+        "registrationLink":"https://lu.ma/Networking_101"
+    },
+    {
+        "title": "Decoding the Secret of '25 दिन में पैसा डबल'",
+        "image": DecodingSecret,
+        "linkedin": "https://www.linkedin.com/in/prajyotm",
+        "registrationLink":"https://lu.ma/Decoding_secret"
+    },
+    {
+        "title": "Serveless on AWS",
+        "image": Serverless,
+        "linkedin": "https://www.linkedin.com/in/kamathdurgadas",
+        "registrationLink":"https://lu.ma/ServerlessOnAws"
+    },
+]
+export default seminarCardData;
---
+++ public/assets/images/Talks/index.js
@@ -0,0 +1,13 @@
+import Career from "./CareerPathsInIT.jpg"
+import DecodingSecret from './DecodingSecret.jpg'
+import Networking from './Networking101.jpg'
+import Serverless from './ServelessOnAWS.jpg'
+import NewComputing from './NewComputing.png'
+
+export {
+    Career,
+    DecodingSecret,
+    Networking,
+    Serverless,
+    NewComputing
+}
