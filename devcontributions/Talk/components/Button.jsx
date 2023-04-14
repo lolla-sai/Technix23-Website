f74e7aa21c15cac23dd75c7c381ceb38894512d0
Jayden Viegas <jaydenviegas57@gmail.com>
2023-04-15 00:30:16 +0530

    Talk Section

---
+++ devcontributions/Talk/components/Button.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Button({ children, className = "", style = {} }) {
+    return (
+        <div
+            className={
+                "bg-[#ffe198] border-2 border-black rounded-full px-4 md:text-2xl text-lg cursor-pointer py-2 text-center inline-block z-20 " +
+                className
+            }
+            style={{
+                boxShadow: "-3px 4px 0 #262626",
+                ...style,
+            }}
+        >
+            {children}
+        </div>
+    );
+}
+
+export default Button;
